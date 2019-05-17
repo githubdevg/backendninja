d5ef6542c4be0b8ac4de8cc7d25ef966abc8cfb7
Diego Vilchis <diego.vilchis@globant.com>
2019-05-17 17:20:37 -0500

    [BackEnd Ninja] Controllers
    - Implementation of controllers

---
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -0,0 +1,41 @@
+package com.udemy.controller;
+
+
+import com.udemy.model.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/example")
+public class ExampleController {
+    public static final String EXAMPLE_VIEW = "example";
+
+    //Primera Forma
+    @GetMapping("/exampleString")
+    public String exampleString(Model model) {
+        model.addAttribute("people", getPeople());
+        return EXAMPLE_VIEW;
+    }
+
+    //Segunda Forma
+    @GetMapping("/exampleMAV")
+    public ModelAndView exampleMAV() {
+        ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+        mav.addObject("people", getPeople());
+        return mav;
+    }
+
+    private List<Person> getPeople() {
+        return Arrays.asList(new Person("Jon", 23), new Person("Mikel", 30),
+                new Person("Eva", 43), new Person("Peter", 18));
+    }
+
+}
+
+
